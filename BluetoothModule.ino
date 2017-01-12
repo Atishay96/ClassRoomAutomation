7569d8f3490e92a13060b2c0eab334968e3d6e4e
Atishay Jain <jainatishay.j@gmail.com>
2017-01-13 01:36:59 +0530

    Add files via upload
    Bluetooth Module consists of Arduino code for controlling led's by Voice control App(on Android) and the other ino file consist of controlling led's by IR sensors.

---
+++ BluetoothModule.ino
@@ -0,0 +1,68 @@
+String voice;
+int
+led1 = 9, //Connect LED 1 To Pin #9
+led2 = 10, //Connect LED 2 To Pin #10
+led3 = 11; //Connect LED 3 To Pin #11
+
+//--------------------------Call A Function-------------------------------// 
+void allon(){
+     digitalWrite(led1, HIGH);//Will turn on LED 1
+     digitalWrite(led2, HIGH);//Will turn on LED 2
+     digitalWrite(led3, HIGH);//Will turn on LED 3
+     
+}
+void alloff(){
+     digitalWrite(led1, LOW);//Will turn off LED 1
+     digitalWrite(led2, LOW);//Will turn off LED 2
+     digitalWrite(led3, LOW);//Will turn off LED 3
+     
+}
+//-----------------------------------------------------------------------// 
+void setup() {
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);//will show the output
+  pinMode(led2, OUTPUT);//will show the output
+  pinMode(led3, OUTPUT);//will show the output
+  
+}
+//-----------------------------------------------------------------------// 
+void loop() {
+  while (Serial.available()){  //Check if there is an available byte to read
+  delay(10); //Delay added to make thing stable
+  char c = Serial.read(); //Conduct a serial read
+  if (c == '#') {break;} //Exit the loop when the # is detected after the word
+  voice += c; //Shorthand for voice = voice + c
+  } 
+  if (voice.length() > 0) {
+    Serial.println(voice);
+//-----------------------------------------------------------------------//   
+  //----------Control Multiple Pins/ LEDs----------// 
+       if(voice == "*all on") {allon();}  //Turn Off All Pins (Call Function)
+  else if(voice == "*all of"){alloff();} //Turn On  All Pins (Call Function)
+  else if(voice == "*all off"){alloff();} //Turn On  All Pins (Call Function)
+ 
+  //----------Turn On One-By-One----------//
+  else if(voice == "*light one on") {digitalWrite(led1, HIGH);}
+  else if(voice == "*light two on") {digitalWrite(led2, HIGH);}
+  else if(voice == "*light to on") {digitalWrite(led2, HIGH);}
+  else if(voice == "*light three on") {digitalWrite(led3, HIGH);}
+  else if(voice == "*light tree on") {digitalWrite(led3, HIGH);}
+  else if(voice == "*light 3 on") {digitalWrite(led3, HIGH);}
+ 
+  //----------Turn Off One-By-One----------//
+  else if(voice == "*light one of") {digitalWrite(led1, LOW);}
+  else if(voice == "*light one off") {digitalWrite(led1, LOW);}
+  else if(voice == "*light two of") {digitalWrite(led2, LOW);}
+  else if(voice == "*light to of") {digitalWrite(led2, LOW);}
+  else if(voice == "*light two off") {digitalWrite(led2, LOW);}
+  else if(voice == "*light to off") {digitalWrite(led2, LOW);}
+  else if(voice == "*light three of") {digitalWrite(led3, LOW);}
+  else if(voice == "*light three off") {digitalWrite(led3, LOW);}
+  else if(voice == "*light 3 of") {digitalWrite(led3, LOW);}
+  else if(voice == "*light 3 off") {digitalWrite(led3, LOW);}
+  else if(voice == "*light tree of") {digitalWrite(led3, LOW);}
+  else if(voice == "*light tree off") {digitalWrite(led3, LOW);}
+ 
+//-----------------------------------------------------------------------// 
+voice="";}} //Reset the variable after initiating
+
---
+++ final_code_testing.ino
@@ -0,0 +1,53 @@
+int k=0;
+const int in = A2 ;
+const int out = A5 ;
+const int led1 = 9;//Connect LED 1 To Pin #9
+const int led2 =10;//Connect LED 2 To Pin #10
+const int led3 =11;//Connect LED 3 To Pin #11
+void setup() {
+  Serial.begin(9600);
+ pinMode (in,INPUT);
+ pinMode (out,INPUT);
+ pinMode (led1,OUTPUT);
+ pinMode (led2,OUTPUT);
+ pinMode (led3,OUTPUT);
+ // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (analogRead(in) >= 220)
+{
+ k++;
+ delay(2000);
+}
+ if (analogRead(out)>=220)
+{
+  k--;
+  delay(2000);
+}
+   if (k <= 0)//there are no students in the class
+  {
+    digitalWrite (led1,LOW);//Will turn off Led 1
+    digitalWrite (led2,LOW);//WIll turn off Led 2
+    Serial.println(k);//Will print current number of students on Serial Monitor
+  }
+    if (k != 0 && k <= 6 )//If students are less than 7 and more than 0
+  {
+    digitalWrite (led1,HIGH);//Will turn on Led 1
+    digitalWrite (led2,LOW);//WIll turn off Led 2
+    Serial.println(k);//Will print current number of students on Serial Monitor
+  }
+  if (k >=7 && k <= 12)//If students are less than 13 and more than 6
+  {
+    digitalWrite (led2,HIGH);//Will turn on Led 2
+    digitalWrite(led3,LOW);//Will turn off Led 3
+    Serial.println(k);//Will print current number of students on Serial Monitor
+  }
+  if (k>=13)//If students are more than or equal to 13
+  {
+    digitalWrite(led3,HIGH);//Will turn on Led 3
+    Serial.println(k);}//Will print current number of students in the class on Serial Monitor
+  }
+
